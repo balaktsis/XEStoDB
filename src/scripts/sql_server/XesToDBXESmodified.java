bad20d822ade83f11559da214a2787c408ee45b0
francesco <francesco@192.168.34.191>
2022-03-14 17:40:31 +0100

    Performance improvement

--- src/scripts/sql_server/XesToDBXESmodified.java
+++ src/scripts/sql_server/XesToDBXESmodified.java
@@ -5,7 +5,6 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
@@ -25,15 +24,15 @@ import org.deckfour.xes.model.XTrace;
 import commons.Commons;
 
 public class XesToDBXESmodified {
-	
+
 	private static final String SCHEMA_NAME = "dbxes_modified";
 	private static final String DB_URL = "jdbc:sqlserver://localhost:1433;databaseName=" + SCHEMA_NAME + ";encrypt=true;trustServerCertificate=true;";
 	private static final String USER = "sa";
 	private static final String PWD = "Riva96_shared_db";
 	private static final String DRIVER_CLASS = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-	
+		
 	private enum Scope {NONE, EVENT, TRACE};
-
+	
 	public static void main(String[] args) {
 		File logFile = Commons.selectLogFile();
 		if (logFile == null) return;
@@ -49,8 +48,8 @@ public class XesToDBXESmodified {
 				// Clearing all data previously contained in the database
 				st.execute("EXEC sp_MSForEachTable 'ALTER TABLE ? NOCHECK CONSTRAINT ALL';");
 				st.execute("EXEC sp_MSForEachTable 'DELETE FROM ?';");
-				st.execute("EXEC sp_MSForEachTable 'ALTER TABLE ? CHECK CHECK CONSTRAINT ALL';");
-				st.execute("EXEC sp_MSforeachtable 'DBCC CHECKIDENT(''?'', RESEED, -1)'");
+				st.execute("EXEC sp_MSForEachTable 'ALTER TABLE ? CHECK CONSTRAINT ALL';");
+				st.execute("EXEC sp_MSforeachtable 'DBCC CHECKIDENT(''?'', RESEED, -1)';");
 				
 				ResultSet resultingId;	// Used to retrieve the id assigned to newly inserted elements in all the tables
 				
@@ -131,14 +130,13 @@ public class XesToDBXESmodified {
 					System.out.println("Log " + (logId+1) + " - Converting attributes");
 					for (XAttribute att : log.getAttributes().values())
 						if (!att.getKey().equals(XConceptExtension.KEY_NAME))
-							populateAttributeAndRelatedTables("log", logId, att, -1, st, Scope.NONE);
+							populateLogAttributeRelatedTables(st, logId, att, -1, Scope.NONE);
 					
 					for (XAttribute att : log.getGlobalTraceAttributes())
-						populateAttributeAndRelatedTables("log", logId, att, -1, st, Scope.TRACE);
+						populateLogAttributeRelatedTables(st, logId, att, -1, Scope.TRACE);
 					
 					for (XAttribute att : log.getGlobalEventAttributes())
-						populateAttributeAndRelatedTables("log", logId, att, -1, st, Scope.EVENT);
-											
+						populateLogAttributeRelatedTables(st, logId, att, -1, Scope.EVENT);
 					
 					int logSize = log.size();
 					for (XTrace trace : log) {
@@ -157,43 +155,59 @@ public class XesToDBXESmodified {
 						
 						st.execute(
 							"INSERT INTO log_has_trace ( log_id, trace_id ) "
-							+ "VALUES ( '" + logId + "', '" + traceId+ "' );"
+							+ "VALUES ( '" + logId + "', '" + traceId + "' );"
 						);
 						
 						for (XAttribute att : trace.getAttributes().values())
 							if (!att.getKey().equals(XConceptExtension.KEY_NAME))
-								populateAttributeAndRelatedTables("trace", traceId, att, -1, st, null);
-						
-						
+								populateTraceAttributeRelatedTables(st, traceId, att, -1);
+												
 						for (XEvent event : trace) {
 							String eventName = Commons.prepareValueForInsertion( XConceptExtension.instance().extractName(event), 250);
 							String eventTransition = Commons.prepareValueForInsertion( XLifecycleExtension.instance().extractTransition(event), 50);
 							
-							SimpleDateFormat sdf = new SimpleDateFormat();
-							sdf.applyPattern("YYYY-MM-dd HH:mm:ss.SSS XXX");
-							Date eventTimestamp = XTimeExtension.instance().extractTimestamp(event);
+							long activityId;
 							
-							String timestampStr = Commons.prepareValueForInsertion( sdf.format(eventTimestamp), 50);
+							try (Statement q = conn.createStatement()) {
+								ResultSet asd = q.executeQuery(
+									"INSERT INTO activity ( name, transition ) "
+									+ "OUTPUT INSERTED.ID "
+									+ "VALUES ( " + eventName + ", " + eventTransition + " );"
+								);
+								asd.next();
+								activityId = Long.parseLong(asd.getString(1));
+							
+							} catch (SQLException e) {
+								ResultSet temp = st.executeQuery("SELECT max(id) FROM activity");
+								temp.next();
+								String lastInsertedSeed = temp.getString(1);
+								st.execute("DBCC CHECKIDENT(activity, RESEED, " + lastInsertedSeed + ");");
+								
+								ResultSet asd = st.executeQuery(
+									"SELECT id FROM activity "
+									+ "WHERE name" + Commons.selectPredicate(eventName) + eventName
+										+ " AND transition" + Commons.selectPredicate(eventTransition) + eventTransition + ";"
+								);
+								asd.next();
+								activityId = Long.parseLong(asd.getString(1));
+							}
+							
+							Date timestampDate = XTimeExtension.instance().extractTimestamp(event);
+							String eventTimestamp = Commons.prepareValueForInsertion( timestampDate.toInstant().toString(), Integer.MAX_VALUE);
 							
 							resultingId = st.executeQuery(
-								"INSERT INTO event ( name, transition, time, event_coll_id ) "
-								+ "OUTPUT INSERTED.ID "
-								+ "VALUES ( " + eventName + ", " + eventTransition + ", " + timestampStr + ", NULL );"
+								"INSERT INTO event ( trace_id, activity_id, event_coll_id, timestp ) "
+								+ "OUTPUT INSERTED.event_id "
+								+ "VALUES ( '" + traceId + "', '" + activityId + "', NULL, " + eventTimestamp + " );"
 							);
-							
 							resultingId.next();
 							long eventId = Long.parseLong(resultingId.getString(1));
-							
-							st.execute(
-								"INSERT INTO trace_has_event ( trace_id, event_id ) "
-								+ "VALUES ( '" + traceId + "', '" + eventId + "' );"
-							);
 							
 							for (XAttribute att : event.getAttributes().values())
 								if (!att.getKey().equals(XConceptExtension.KEY_NAME)
 										&& !att.getKey().equals(XLifecycleExtension.KEY_TRANSITION)
 										&& !att.getKey().equals(XTimeExtension.KEY_TIMESTAMP))
-									populateAttributeAndRelatedTables("event", eventId, att, -1, st, null);
+									populateEventAttributeRelatedTables(st, eventId, att, -1);
 						}
 					}
 				}
@@ -208,14 +222,95 @@ public class XesToDBXESmodified {
 		}
 	}
 	
-	private static void populateAttributeAndRelatedTables(
-			String relatedTableName, 
-			long relatedTableId, 
-			XAttribute att, 
-			long parentId, 
-			Statement stmt, 
+	private static void populateLogAttributeRelatedTables(
+			Statement stmt,
+			long logId,
+			XAttribute att,
+			long attParentId,
 			Scope scope) throws SQLException {
 		
+		long attId = insertAttributeInTable(stmt, att, attParentId);
+		
+		String value = Commons.prepareValueForInsertion(att.toString(), 250);
+		
+		String scopeStr = scope.equals(Scope.NONE) ? null : scope.toString().toLowerCase();
+		scopeStr = Commons.prepareValueForInsertion(scopeStr, 50);
+		
+		ResultSet existenceCheck = stmt.executeQuery(
+			"SELECT 1 FROM log_has_attribute "
+			+ "WHERE log_id = '" + logId + "'"
+				+ " AND attr_id = '" + attId + "'"
+				+ " AND value" + Commons.selectPredicate(value) + value
+		);
+			
+		if (!existenceCheck.next())
+			stmt.execute(
+				"INSERT INTO log_has_attribute ( log_id, attr_id, value, scope ) "
+				+ "VALUES ( '" + logId + "', '" + attId + "', " + value + ", " + scopeStr + " );"
+			);
+		
+		long newParentId = attId;
+		for (XAttribute nested : att.getAttributes().values())
+			populateLogAttributeRelatedTables(stmt, logId, nested, newParentId, scope);
+	}
+	
+	private static void populateTraceAttributeRelatedTables(
+			Statement stmt,
+			long traceId,
+			XAttribute att,
+			long attParentId) throws SQLException {
+		
+		long attId = insertAttributeInTable(stmt, att, attParentId);
+		
+		String value = Commons.prepareValueForInsertion(att.toString(), 250);
+		
+		ResultSet existenceCheck = stmt.executeQuery(
+				"SELECT 1 FROM trace_has_attribute "
+				+ "WHERE trace_id = '" + traceId + "'"
+					+ " AND attr_id = '" + attId + "'"
+					+ " AND value" + Commons.selectPredicate(value) + value + ";"
+			);
+		
+		if (!existenceCheck.next())
+			stmt.execute(
+				"INSERT INTO trace_has_attribute ( trace_id, attr_id, value ) "
+				+ "VALUES ( '" + traceId + "', '" + attId + "', " + value + " );"
+			);
+		
+		long newParentId = attId;
+		for (XAttribute nested : att.getAttributes().values())
+			populateTraceAttributeRelatedTables(stmt, traceId, nested, newParentId);
+	}
+	
+	private static void populateEventAttributeRelatedTables(
+			Statement stmt,
+			long eventId,
+			XAttribute att,
+			long attParentId) throws SQLException {
+		
+		long attId = insertAttributeInTable(stmt, att, attParentId);
+		
+		String value = Commons.prepareValueForInsertion(att.toString(), 250);
+				
+		ResultSet existenceCheck = stmt.executeQuery(
+				"SELECT 1 FROM event_has_attribute "
+				+ "WHERE event_id = '" + eventId + "'"
+					+ " AND attr_id = '" + attId + "'"
+					+ " AND value" + Commons.selectPredicate(value) + value + ";"
+			);
+		
+		if (!existenceCheck.next())
+			stmt.execute(
+				"INSERT INTO event_has_attribute ( event_id, attr_id, value ) "
+				+ "VALUES ( '" + eventId + "', '" + attId + "', " + value + " );"
+			);
+		
+		long newParentId = attId;
+		for (XAttribute nested : att.getAttributes().values())
+			populateTraceAttributeRelatedTables(stmt, eventId, nested, newParentId);
+	}
+	
+	private static long insertAttributeInTable(Statement stmt, XAttribute att, long parentId) throws SQLException {
 		String type;
 		if (att instanceof XAttributeBoolean)
 			type = "'boolean'";
@@ -227,7 +322,6 @@ public class XesToDBXESmodified {
 			type = "'literal'";
 
 		String key = Commons.prepareValueForInsertion(att.getKey(), 50);
-		String value = Commons.prepareValueForInsertion(att.toString(), 250);
 		
 		String extId;
 		if (att.getExtension() != null) {
@@ -256,15 +350,14 @@ public class XesToDBXESmodified {
 			"SELECT id FROM attribute "
 			+ "WHERE type" + Commons.selectPredicate(type) + type
 				+ " AND [key]" + Commons.selectPredicate(key) + key
-				+ " AND [value]" + Commons.selectPredicate(value) + value
 				+ " AND ext_id" + Commons.selectPredicate(extId) + extId
 				+ " AND parent_id" + Commons.selectPredicate(parentIdStr) + parentIdStr + ";"
 		);
 		
 		if (!resultingId.next()) {	// Means that the attribute isn't present yet and should be inserted
-			List<String> values = List.of(type, key, value, extId, parentIdStr);
+			List<String> values = List.of(type, key, extId, parentIdStr);
 			resultingId = stmt.executeQuery(
-				"INSERT INTO attribute ( type, [key], [value], ext_id, parent_id ) "
+				"INSERT INTO attribute ( type, [key], ext_id, parent_id ) "
 				+ "OUTPUT INSERTED.ID "
 				+ "VALUES ( " + String.join(", ", values) + " );"
 			);
@@ -272,53 +365,6 @@ public class XesToDBXESmodified {
 			resultingId.next();
 		}
 		
-		long attId = Long.valueOf(resultingId.getString(1));
-		
-		ResultSet existenceCheck = stmt.executeQuery(
-			"SELECT 1 FROM " + relatedTableName+"_has_attribute "
-			+ "WHERE " + relatedTableName+"_id = '" + relatedTableId + "' AND attr_id = '" + attId + "' ;"
-		);
-		
-		boolean isAttributeExisting = existenceCheck.next();
-		
-		switch (relatedTableName) {
-		case "log":
-			String scopeStr;
-			if (scope.equals(Scope.NONE))
-				scopeStr = null;
-			else
-				scopeStr = scope.toString().toLowerCase();
-			
-			scopeStr = Commons.prepareValueForInsertion(scopeStr, 50);
-			
-			if (!isAttributeExisting)
-				stmt.execute(
-					"INSERT INTO log_has_attribute ( log_id, attr_id, scope ) "
-					+ "VALUES ( '" + relatedTableId + "', '" + attId + "', " + scopeStr + " );"
-				);
-			
-			break;
-
-		case "trace":
-			if (!isAttributeExisting)
-				stmt.execute(
-					"INSERT INTO trace_has_attribute ( trace_id, attr_id ) "
-					+ "VALUES ( '" + relatedTableId + "', '" + attId + "' );"
-				);
-			break;
-			
-		case "event":
-			if (!isAttributeExisting)
-				stmt.execute(
-					"INSERT INTO event_has_attribute ( event_id, attr_id ) "
-					+ "VALUES ( '" + relatedTableId + "', '" + attId + "' );"
-				);
-			break;
-		}
-		
-		
-		long newParentId = attId;
-		for (XAttribute nested : att.getAttributes().values())
-			populateAttributeAndRelatedTables(relatedTableName, relatedTableId, nested, newParentId, stmt, scope);
+		return Long.valueOf(resultingId.getString(1));
 	}
 }
